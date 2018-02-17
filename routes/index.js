37d0f52f0eb31fe75897d525a28c546cc6409cdb
jainish shah <jainishshah@yahoo.com>
2018-02-17 11:32:37 -0800

    Hue apis

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var Hue = require('philips-hue');
+var router = express.Router();
+var hue = new Hue();
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+    console.log("Running Hue APIs");
+    hue.getBridges()
+        .then(function(bridges){
+            console.log(bridges);
+            var bridge = bridges[0]; // use 1st bridge
+            console.log("bridge: "+bridge);
+            return hue.auth(bridge);
+        })
+        .then(function(username){
+            console.log("username: "+username);
+
+            // controll Hue lights
+            hue.light(1).on();
+            hue.light(2).off();
+            hue.light(3).setState({hue: 50000, sat: 200, bri: 90});
+        })
+        .catch(function(err){
+            console.error(err.stack || err);
+        });
+});
+
+module.exports = router;

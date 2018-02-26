648f6944c39eb368fe84623bb4b1b052aaae4d4c
jainish shah <jainishshah@yahoo.com>
2018-02-26 09:06:42 -0800

    added sense

--- routes/light.js
+++ routes/light.js
@@ -3,22 +3,14 @@ var sense = require("sense-hat-led").sync;
 var router = express.Router();
 light();
 
-function image() {
-    console.log("Printing image");
-    sense.loadImage("images/logo.png");
-    sense.clear();
-}
-
-image();
 function light() {
     console.log("Printing numbers");
     for (var i = 10; i > 0; i--){
-        sense.showLetter(i.toString());
-        sense.sleep(0.6);
+        sense.showLetter(i.toString(), [124, 252, 0]);
+        sense.sleep(0.5);
     }
     sense.clear();
+    light();
 }
-
-
 
 module.exports = router;

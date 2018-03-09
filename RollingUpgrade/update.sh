786a041074d0cc2d15e1b4fc64d8e457f8749ec5
jainish shah <jainishshah@yahoo.com>
2018-03-09 08:46:46 -0800

    changed docker repo

--- RollingUpgrade/update.sh
+++ RollingUpgrade/update.sh
@@ -7,9 +7,9 @@ else
       version="$1"
 fi
 function rollingUpgrade {
-    docker pull shahbrothers-docker-registry.bintray.io/node-version:${version}>> update.log
+    docker pull jfrog-int-docker-open-docker.bintray.io/iot-demo:${version}>> update.log
     docker rm -f pi>> update.log
-    docker run -d --name pi -it -p 3000:3000 --privileged --restart always -v /home/pi/bridge.json:/usr/src/app/bridge.json shahbrothers-docker-registry.bintray.io/node-version:${version}>> update.log
+    docker run -d --name pi -it -p 3000:3000 --privileged --restart always -v /home/pi/bridge.json:/usr/src/app/bridge.json jfrog-int-docker-open-docker.bintray.io/iot-demo:${version}>> update.log
     if [ "$(docker ps -a | grep pi)" ]; then
        echo "ok"
     fi
